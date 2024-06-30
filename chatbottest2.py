f76974e323bfffbee213d44b5a5dc5d541948f2b
lyh9003 <lyh9003@gmail.com>
2024-06-30 22:32:50 +0900

    Update chatbottest2.py

--- chatbottest2.py
+++ chatbottest2.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("친근한 챗봇")
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+client = OpenAI(api_key=st.secrets["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"])
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
