9efe0f918119ce86a4a65c06470c5ed802872731
lyh9003 <lyh9003@gmail.com>
2024-07-29 17:56:50 +0900

    Update streamlit_email_Test.py

--- streamlit_email_Test.py
+++ streamlit_email_Test.py
@@ -5,6 +5,18 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
+def change_chatbot_style():
+    # Set style of chat input so that it shows up at the bottom of the column
+    chat_input_style = """
+    <style>
+        .stChatInput {
+          position: fixed;
+          bottom: 3rem;
+          width: calc(100% - 2rem);
+        }
+    </style>
+    """
+    st.markdown(chat_input_style, unsafe_allow_html=True)
 
 st.title("논문 Survey")
 st.write("음식과 관련된 문장을 적어보세요")
@@ -91,24 +103,24 @@ with col2:
                     st.markdown(message["content"])
 
     # 메시지 입력 창이 화면 맨 아래에 고정되도록 설정
-    input_container = st.container()
-    with input_container:
-        if prompt := st.chat_input("메시지를 입력하세요..."):
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            with st.chat_message("user"):
-                st.markdown(prompt)
+    change_chatbot_style()
+    if prompt := st.chat_input("메시지를 입력하세요..."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-            with st.chat_message("assistant"):
-                stream = client.chat.completions.create(
-                    model=st.session_state["openai_model"],
-                    messages=[
-                        {"role": m["role"], "content": m["content"]}
-                        for m in st.session_state.messages
-                    ],
-                    stream=True,
-                )
-                response = st.write_stream(stream)
-            st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
 
 def send_email(subject, body, to_email="rollingfac@naver.com"):
 
